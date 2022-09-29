5f0c705a982b2487231c24cdfb59ff31a9f1358d
Shuhei Nitta <huisintheta@gmail.com>
2022-09-29 21:57:45 +0900

    ✨ Add Kubernetes dashboard (#1)

--- scripts/install-microk8s.sh
+++ scripts/install-microk8s.sh
@@ -4,8 +4,23 @@ set -euC
 set -o pipefail
 set -x
 
+SCRIPTS_DIR=$(dirname "$0")
+SERVICES_DIR=$(realpath "$SCRIPTS_DIR"/../services)
+
 sudo snap install microk8s --classic
-sudo snap install kubectl --classic
 sudo usermod -aG microk8s "$USER"
 sudo chown -fR "$USER" ~/.kube
-microk8s config > ~/.kube/config
+sudo snap install kubectl --classic
+
+if [ ! -f ~/.kube/config ]; then
+    microk8s config > ~/.kube/config
+fi
+
+# Install addons
+microk8s enable dns dashboard
+microk8s enable ingress hostpath-storage observability
+
+SERVICE_FILENAME="kubernetes-dashboard.service"
+if [ ! -f /etc/systemd/system/"$SERVICE_FILENAME" ]; then
+    sudo ln -s "$SERVICES_DIR/$SERVICE_FILENAME" /etc/systemd/system/"$SERVICE_FILENAME"
+fi
