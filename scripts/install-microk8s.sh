76b6d626a73b81b478f60ee1b46443d716ff9810
Shuhei Nitta <huisintheta@gmail.com>
2022-09-29 21:00:25 +0900

    Update install-microk8s.sh

--- scripts/install-microk8s.sh
+++ scripts/install-microk8s.sh
@@ -7,15 +7,10 @@ set -x
 SCRIPTS_DIR=$(dirname "$0")
 SERVICES_DIR=$(realpath "$SCRIPTS_DIR"/../services)
 
-if [ -z "$(which microk8s)" ]; then
-    sudo snap install microk8s --classic
-    sudo usermod -aG microk8s "$USER"
-    sudo chown -fR "$USER" ~/.kube
-fi
-
-if [ -z "$(which kubectl)" ]; then
-    sudo snap install kubectl --classic
-fi
+sudo snap install microk8s --classic
+sudo usermod -aG microk8s "$USER"
+sudo chown -fR "$USER" ~/.kube
+sudo snap install kubectl --classic
 
 if [ ! -f ~/.kube/config ]; then
     microk8s config > ~/.kube/config
@@ -23,8 +18,8 @@ fi
 
 # Install addons
 microk8s enable dns dashboard ingress hostpath-storage observability
-SERVICE_FILENAME="kubernetes-dashboard.service"
 
+SERVICE_FILENAME="kubernetes-dashboard.service"
 if [ ! -f /etc/systemd/system/"$SERVICE_FILENAME" ]; then
     sudo ln -s "$SERVICES_DIR/$SERVICE_FILENAME" /etc/systemd/system/"$SERVICE_FILENAME"
 fi
