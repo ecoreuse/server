90a21a45aeb35eb859a8dce88015757476bbe24c
Shuhei Nitta <huisintheta@gmail.com>
2022-09-29 20:42:18 +0900

    Update install-crontabs.sh

--- scripts/install-crontabs.sh
+++ scripts/install-crontabs.sh
@@ -4,5 +4,8 @@ set -euC
 set -o pipefail
 set -x
 
-sudo crontab crontabs/root
-crontab crontabs/user
+SCRIPTS_DIR=$(dirname "$0")
+CRONTABS_DIR=$(realpath "$SCRIPTS_DIR"/../crontabs)
+
+sudo crontab "$CRONTABS_DIR"/root
+crontab "$CRONTABS_DIR"/user
