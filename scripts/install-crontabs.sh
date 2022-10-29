82414c131dda57c8ffdec246d5e726e239edae75
Shuhei Nitta <huisintheta@gmail.com>
2022-10-30 06:06:21 +0900

    ✨ Add apt-upgrade.sh (#4)

--- scripts/install-crontabs.sh
+++ scripts/install-crontabs.sh
@@ -9,3 +9,8 @@ CRONTABS_DIR=$(realpath "$SCRIPTS_DIR"/../crontabs)
 
 sudo crontab "$CRONTABS_DIR"/root
 crontab "$CRONTABS_DIR"/user
+
+# apt upgrade
+sudo cp "$SCRIPTS_DIR"/apt-upgrade.sh /usr/local/bin/
+sudo chmod 544 /usr/local/bin/apt-upgrade.sh
+sudo chown root:root /usr/local/bin/apt-upgrade.sh
