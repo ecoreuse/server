ca50673115c38f5b36e4fd0ff53a61ac2da3d6d8
ecoreuse-master <ecoreuse9@gmail.com>
2023-11-26 10:54:18 +0000

    Update apt-upgrade.sh

--- scripts/apt-upgrade.sh
+++ scripts/apt-upgrade.sh
@@ -4,10 +4,6 @@ set -euC
 set -o pipefail
 set -x
 
-UPGRADE_LOG_FILE="/dev/stdout"
-AUTOREMOVE_LOG_FILE="/dev/stdout"
-
-export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get upgrade -y | tee -a "$UPGRADE_LOG_FILE"
-sudo apt-get autoremove -y | tee -a "$AUTOREMOVE_LOG_FILE"
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
